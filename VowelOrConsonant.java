cec6597562642cde5e6d80a27ffa5b705619afaa
thiyagu <thiyagasaravan97@gmail.com>
2017-07-10 11:18:24 +0530

    Update VowelOrConsonant.java

--- VowelOrConsonant.java
+++ VowelOrConsonant.java
@@ -5,7 +5,7 @@ public class VowelOrConsonant {
     	 char c=(char)br.read();
     	 if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||
 			c=='A'||c=='E'||c=='I'||c=='O'||c=='U'){
-    		 System.out.println(" Vowels");
+    		 System.out.println("Vowels");
     	 }
     	 else
     		 System.out.println("Consonant");
