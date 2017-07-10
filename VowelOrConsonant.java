b10e5ff29729a1b958af403089fd3e9c0519e66d
thiyagu <thiyagasaravan97@gmail.com>
2017-07-10 11:15:49 +0530

    Update VowelOrConsonant.java

--- VowelOrConsonant.java
+++ VowelOrConsonant.java
@@ -1,1 +1,14 @@
+import java.io.*;
+public class VowelOrConsonant {
+     public static void main(String[] args)throws IOException{
+    	 BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    	 char c=(char)br.read();
+    	 if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||
+			c=='A'||c=='E'||c=='I'||c=='O'||c=='U'){
+    		 System.out.println(" Vowels");
+    	 }
+    	 else
+    		 System.out.println("Consonant");
+     }
+}
 
