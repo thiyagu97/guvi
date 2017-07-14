b36b8cc5b5766ecec67b2c62ee0feda2500a0996
thiyagu <thiyagasaravan97@gmail.com>
2017-07-14 14:06:07 +0530

    Create ExactlyTwice2.java

---
+++ ExactlyTwice2.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class ExactlyTwice2{
+public static void main( String[] args){
+	   Scanner sc= new Scanner(System.in);
+	   int c=0,ans=0;
+	   System.out.println("Ente the size the array");
+	   int n=sc.nextInt();
+	   int[] a=new int[n];
+	   System.out.println("Ente the values");
+	   for(int i=0;i<n;i++){
+		   a[i]=sc.nextInt();
+	   }
+	   for(int j=0;j<n;j++){
+		   c=0;
+		   for(int k=0;k<n;k++){
+			   if(a[j]==a[k]){
+				   c++;
+			   }
+		   }if(c==1){
+			   ans=a[j];
+		   }
+	   }
+		   System.out.println(ans);
+   }
+}
