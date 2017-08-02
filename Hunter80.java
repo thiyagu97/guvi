e7c344a5074c4c0abc3a091e3daea0045b81b9bd
thiyagu <thiyagasaravan97@gmail.com>
2017-08-02 15:18:44 +0530

    Create Hunter80.java

---
+++ Hunter80.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Hunter80 {
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number");
+		int a=sc.nextInt();
+		int count=0;
+		ArrayList<Integer> list=new ArrayList<Integer>();
+		for(int i=1;i<=a;i++){
+			if(i<=10){
+				list.add(i);
+			}
+			else{
+				String num=String.valueOf(i);
+				char[] c=num.toCharArray();
+				count=0;
+				for(int i1=0;i1<c.length-1;i1++){
+					int n1=c[i1];int n2=c[i1+1];
+					if(Math.abs(n1-n2)==1){
+						count++;
+						}
+					}
+				if(count==((c.length)-1))
+					list.add(i);
+			}
+		}
+		for(Integer ans:list)
+		System.out.println(ans);
+	}
+}
